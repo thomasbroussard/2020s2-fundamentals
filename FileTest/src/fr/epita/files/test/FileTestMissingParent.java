c45e1f51b6e78133c6322cadd2ad3814166bae9e
thomas <thomas.broussard@local.local>
2020-11-09 15:49:55 +0100

    * test on file creation

---
+++ FileTest/src/fr/epita/files/test/FileTestMissingParent.java
@@ -0,0 +1,28 @@
+package fr.epita.files.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileTestMissingParent {
+	
+	public static void main(String[] args) throws IOException {
+		File file = new File("S:/tmp/test/java/file.txt");
+		
+		boolean exists = file.exists();
+		System.out.println("file exists : " + exists);
+		
+	
+		
+		File parentFile = file.getParentFile();
+		System.out.println("is the parent file a directory? : " + parentFile.isDirectory());
+		
+		parentFile.mkdirs();
+		
+		System.out.println("is the parent file a directory? : " + parentFile.isDirectory());
+		
+		file.createNewFile();
+		
+		System.out.println("file exists : " + file.exists());
+	}
+
+}
