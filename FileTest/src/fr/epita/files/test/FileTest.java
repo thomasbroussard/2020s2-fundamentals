15b7b097eaaeff6a6383d9343ae9cc57e6d14b8e
thomas <thomas.broussard@local.local>
2020-11-16 15:00:41 +0100

    * upd : file reading

--- FileTest/src/fr/epita/files/test/FileTest.java
+++ FileTest/src/fr/epita/files/test/FileTest.java
@@ -3,23 +3,41 @@ package fr.epita.files.test;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 
 public class FileTest {
-	
+
 	public static void main(String[] args) throws IOException {
+		// fileWrite();
+		List<String> listOfLines = Files.readAllLines(new File("test.log").toPath());
+		System.out.println(listOfLines);
+
+		for (String line : listOfLines) {
+			line.trim();// remove trailing and starting spaces
+			line.replaceAll("\"", ""); // remove double quotes from the whole string.
+			try {
+				Integer.valueOf(line);
+			} catch (NumberFormatException nfe) {
+				nfe.printStackTrace();
+			}
+		}
+
+	}
+
+	private static void fileWrite() throws IOException {
 		File file = new File("./test.log");
 		boolean fileExists = file.exists();
 		System.out.println("file exists : " + fileExists);
-		
+
 		if (!fileExists) {
 			file.createNewFile();
 		}
 		FileWriter fileWriter = new FileWriter(file, true);
 		String lineSep = System.getProperty("line.separator");
 		fileWriter.append("this is a test" + lineSep);
-		
+
 		fileWriter.close();
-		
 	}
 
 }
